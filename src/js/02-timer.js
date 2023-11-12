22133b7d411a4a1349492f8e4fcc17ab87486a8c
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-12 17:39:09 +0200

    update02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,3 @@
-
 import flatpickr from 'flatpickr';
 
 import 'flatpickr/dist/flatpickr.min.css';
@@ -11,10 +10,40 @@ import 'flatpickr/dist/flatpickr.min.css';
 // datetime - picker
 
 const refs = {
-  startBtn: document.querySelector('button[data-start]'),
+  startBtn: document.querySelector('[data-start]'),
+  timerFieldDays: document.querySelector('[data-days]'),
+  timerFieldHours: document.querySelector('[data-hours]'),
+  timerFieldMinites: document.querySelector('[data-minutes]'),
+  timerFieldSeconds: document.querySelector('[data-seconds]'),
 };
 console.log(refs.startBtn);
+console.log(refs.timerFieldDays);
+console.log(refs.timerFieldHours);
+console.log(refs.timerFieldMinites);
+console.log(refs.timerFieldSeconds);
 
-refs.startBtn.addEventListener('click', () => {
-    console.log("rrrrrrrrrr");
-});
+// refs.timerFieldDays.textContent = '25';
+
+refs.startBtn.addEventListener('click', onTimerStart);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    const currentDate = new Date();
+    const selectedDate = selectedDates[0];
+    if (selectedDate < currentDate) {
+      refs.startBtn.disabled = true;
+      window.alert("Please choose a date in the future");
+    } else {refs.startBtn.disabled = false;
+    }
+  },
+};
+
+const fp = flatpickr('#datetime-picker', options);
+
+
+function onTimerStart() {}
