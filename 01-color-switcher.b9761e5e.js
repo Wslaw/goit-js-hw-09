51e08aecd70475110aac63c329127c4b460825a5
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-12 19:19:40 +0000

    Deploying to gh-pages from @ Wslaw/goit-js-hw-09@2133bcd03a746298e163b4426ca5d9fc636bebdb 🚀

---
+++ 01-color-switcher.b9761e5e.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};var n=null;t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,n=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.b9761e5e.js.map
