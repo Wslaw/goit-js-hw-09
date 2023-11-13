5cfe4275a41f48ebdf6393845dd33c1dbf22b189
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-13 12:38:35 +0200

    3styleEnd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,8 +12,8 @@ function createPromise(position, delay) {
 
 createPromise(2, 1500)
   .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   });
